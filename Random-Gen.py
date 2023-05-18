1fe8b4cb68d6986f8e0ee3e6e79a12509cf6a311
Someone-Falcon <131703594+Someone-Falcon@users.noreply.github.com>
2023-05-18 21:46:31 +0200

    Create Random-Gen.py

---
+++ Random-Gen.py
@@ -0,0 +1,8 @@
+import random
+import math
+import os
+
+def generate():
+	random_seed = len(os.urandom(random.range(48)) / len(os.urandom(24))
+	random = round(math.abs(random_seed * random.random math.pow(random.range(4))))
+	return random
