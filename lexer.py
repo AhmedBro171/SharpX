7bd317c4aa74135d560600d620a93aef4962fb13
notcl4y14 <128468209+notcl4y14@users.noreply.github.com>
2023-05-18 23:10:41 +0300

    Create lexer.py

---
+++ lexer.py
@@ -0,0 +1,12 @@
+class Lexer:
+  def __init__(self, code):
+    self.code = code
+    self.pos = -1
+    self.char = None
+  def advance():
+    self.pos += 1
+    self.char = self.code[self.pos]
+  # will finish this later or someone finish it if wants
+  # the tokenize() function
+
+
