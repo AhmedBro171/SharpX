15a276f6b69b661b61dd9106e4696dbf8f92078f
Someone-Falcon <131703594+Someone-Falcon@users.noreply.github.com>
2023-05-18 23:17:01 +0200

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,23 @@
+# Mathematics
+
+import math
+
+def pow(num1, power):
+	return math.pow(num1, power)
+def abs(num):
+	return math.abs(num)
+def cos(num):
+	return math.cos(num)
+def sin(num):
+	return math.sin(num)
+def add(num1, num2):
+	add_op = str(int(num1 + num2))
+	return add_op
+def sub(num, num2):
+	sub_op = str(int(num1 - num2))
+	return sub_op
+def mult(nul1, m):
+	m_op = str(int(nul1 * m))
+	return m_op
+
+# I will continue tomorrow.
