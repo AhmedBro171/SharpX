a2113b20966aa23c465a63db6c79df93babb4b5f
notcl4y14 <128468209+notcl4y14@users.noreply.github.com>
2023-05-18 22:38:57 +0300

    Create token.py

---
+++ token.py
@@ -0,0 +1,11 @@
+# i hope i won't get addicted to python while writing this ig lol
+
+TokenType_plus = "plus"
+TokenType_minus = "minus"
+TokenType_multiply = "multiply"
+TokenType_divide = "divide"
+
+class Token:
+  def __init__(self, type, value):
+    self.type = type
+    self.value = value
