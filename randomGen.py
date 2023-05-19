e3afcafd994c432ba8024ca853e8f355667a194b
notcl4y14 <128468209+notcl4y14@users.noreply.github.com>
2023-05-19 14:57:56 +0300

    Update randomGen.py

--- randomGen.py
+++ randomGen.py
@@ -4,5 +4,6 @@ import os
 
 def generate():
 	random_seed = len(os.urandom(random.range(48)) / len(os.urandom(24))
-	random = round(math.abs(random_seed * random.random math.pow(random.range(4))))
-	return random
+	# "random" variable already exists and it's a function, so i renamed it to "randomval"
+	randomval = round(math.abs(random_seed * random.random math.pow(random.range(4))))
+	return randomval
