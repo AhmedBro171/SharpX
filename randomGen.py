beb204ae2a8a2c963123ca55a23259e6b35ec01c
notcl4y14 <128468209+notcl4y14@users.noreply.github.com>
2023-05-19 15:05:35 +0300

    Update randomGen.py

--- randomGen.py
+++ randomGen.py
@@ -4,6 +4,5 @@ import os
 
 def generate():
 	random_seed = len(os.urandom(random.range(48)) / len(os.urandom(24))
-	# a comma was forgotten here ----------------------v
-	random = round(math.abs(random_seed * random.random, math.pow(random.range(4))))
+	random = round(math.abs(random_seed * random.random math.pow(random.range(4))))
 	return random
